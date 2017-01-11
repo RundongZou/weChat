b2fb8499b226bfff0c996403b183b2daf80e1b4c
Rundong Zou <1809638500@qq.com>
2017-01-11 23:15:14 +0800

    xcx first

---
+++ xcx/pages/list/list.js
@@ -0,0 +1,59 @@
+// pages/list/list.js
+Page({
+  data: {
+        list_data: []
+      },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    var _this = this;
+    //數據請求
+    wx.request({
+      data: {
+
+      },
+      //http://datainfo.duapp.com/shopdata/getGoods.php
+      // url: "https://liaman.duapp.com/footerball/mock/list.json",
+      url: "http://datainfo.duapp.com/shopdata/getGoods.php?callback=",
+      header: {
+        'content-type': "application/json"
+      },
+      dataType: "json",
+      // rp->response
+      success: function (rp) {
+          var _data = JSON.parse(rp.data.slice(1, rp.data.length-1));
+          console.log(JSON.parse(rp.data.slice(1, rp.data.length-1)));
+          //var _data = rp.data;
+          // 數據請求成功以後，setData
+          _this.setData({
+            list_data: _data 
+          })
+      }
+    })
+
+   
+
+  },
+
+  itemTap: function (ev) {
+    console.log("tap");
+    console.log(ev);
+    console.log(ev.currentTarget.id);
+
+    wx.navigateTo({
+      url: '../detail/detail?id=' + ev.currentTarget.id
+    })
+  },
+
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
